7262c6be0ccea3c5c85b0e90bb862f9aa05dc6fe
Andres Mena <ballerlikemike23@gmail.com>
2018-12-27 20:44:56 -0500

    Looping Over Arrays

--- todo.js
+++ todo.js
@@ -3,9 +3,13 @@ const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exerci
 // Delete the 3rd item
 todos.splice(2, 1)
 // Add a new item onto the end
-todos.push('This is a new todo')
+todos.push('Buy Coffee')
 // Remove the first item from the list
 todos.shift()
 
 console.log(`You have ${todos.length} todos!`)
-console.log(todos)
+
+todos.forEach(function (todo, index) {
+  const num = index + 1
+  console.log(`${num}. ${todo}`)
+})
