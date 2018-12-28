c5a6aa633ca90ea49b9693a8c7b3f0b68d729a8d
Andres Mena <ballerlikemike23@gmail.com>
2018-12-27 20:35:45 -0500

    Manipulating Arrays with Methods

--- todo.js
+++ todo.js
@@ -1,5 +1,11 @@
 const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
 
+// Delete the 3rd item
+todos.splice(2, 1)
+// Add a new item onto the end
+todos.push('This is a new todo')
+// Remove the first item from the list
+todos.shift()
+
 console.log(`You have ${todos.length} todos!`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(todos)
