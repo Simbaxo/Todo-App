527dc5135e11c8fcd222d42a8b1a9ce874919d29
Andres Mena <ballerlikemike23@gmail.com>
2018-12-30 22:08:18 -0500

    Hide Completed Todos

--- todo.js
+++ todo.js
@@ -47,12 +47,15 @@ const getThingsToDo = function (todos) {
 sortTodos(todos)
 
 const filters = {
-  searchText: ''
+  searchText: '',
+  hideCompleted: false
 }
 
 const renderTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+    return searchTextMatch && hideCompletedMatch
   })
 
   const incompleteTodos = filteredTodos.filter(function (todo) {
@@ -62,7 +65,7 @@ const renderTodos = function (todos, filters) {
   document.querySelector('#todos').innerHTML = ''
 
   const summary = document.createElement('h2')
-  summary.textContent = `You have ${incompleteTodos.length} left`
+  summary.textContent = `You have ${incompleteTodos.length} todos left`
   document.querySelector('#todos').appendChild(summary)
 
   filteredTodos.forEach(function (todo) {
@@ -87,4 +90,9 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
   })
   renderTodos(todos, filters)
   e.target.elements.text.value = ''
+})
+
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+  filters.hideCompleted = e.target.checked
+  renderTodos(todos, filters)
 })
