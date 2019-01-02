85a8b5f8f480889e3388c9764e7d3aac399fe6f5
Andres Mena <ballerlikemike23@gmail.com>
2019-01-01 21:50:04 -0500

    Refactor app by creating new file for js functions

---
+++ todo-functions.js
@@ -0,0 +1,79 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = function () {
+  const todosJSON = localStorage.getItem('todos')
+
+  if (todosJSON !== null) {
+    return JSON.parse(todosJSON)
+  } else {
+    return []
+  }
+}
+
+// Save todos to localStorage
+const saveTodos = function (todos) {
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based on filters
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+    return searchTextMatch && hideCompletedMatch
+  })
+
+  const incompleteTodos = filteredTodos.filter(function (todo) {
+    return !todo.completed
+  })
+
+  document.querySelector('#todos').innerHTML = ''
+  document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+
+  filteredTodos.forEach(function (todo) {
+    document.querySelector('#todos').appendChild(generateTodoDOM(todo))
+  })
+}
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = function (todo) {
+  const p = document.createElement('p')
+  p.textContent = todo.text
+  return p
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function (incompleteTodos) {
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incompleteTodos.length} todos left`
+  return summary
+}
+
+// Sort Todos
+const sortTodos = function (todo) {
+  todo.sort(function (a, b) {
+    if (!a.completed && b.completed) {
+      return -1
+    } else if (!b.completed && a.completed) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+}
+
+// Create function to remove a todo by text value
+const deleteTodo = function (todos, todoText) {
+  const index = todos.findIndex(function (todo) {
+    return todo.text.toLowerCase() === todoText.toLowerCase()
+  })
+  if (index > -1) {
+    todos.splice(index, 1)
+  }
+}
+
+// Find Uncompleted Todos
+const getThingsToDo = function (todos) {
+  return todos.filter(function (todo) {
+    return !todo.completed
+  })
+}
--- todo.js
+++ todo.js
@@ -1,69 +1,8 @@
-let todos = []
-
-const sortTodos = function (todo) {
-  todo.sort(function (a, b) {
-    if (!a.completed && b.completed) {
-      return -1
-    } else if (!b.completed && a.completed) {
-      return 1
-    } else {
-      return 0
-    }
-  })
-}
-
-// Create function to remove a todo by text value
-const deleteTodo = function (todos, todoText) {
-  const index = todos.findIndex(function (todo) {
-    return todo.text.toLowerCase() === todoText.toLowerCase()
-  })
-  if (index > -1) {
-    todos.splice(index, 1)
-  }
-}
-
-// Find Uncompleted Todos
-const getThingsToDo = function (todos) {
-  return todos.filter(function (todo) {
-    return !todo.completed
-  })
-}
-
-sortTodos(todos)
+let todos = getSavedTodos()
 
 const filters = {
   searchText: '',
   hideCompleted: false
-}
-
-const todosJSON = localStorage.getItem('todos')
-
-if (todosJSON !== null) {
-  todos = JSON.parse(todosJSON)
-}
-
-const renderTodos = function (todos, filters) {
-  const filteredTodos = todos.filter(function (todo) {
-    const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-    return searchTextMatch && hideCompletedMatch
-  })
-
-  const incompleteTodos = filteredTodos.filter(function (todo) {
-    return !todo.completed
-  })
-
-  document.querySelector('#todos').innerHTML = ''
-
-  const summary = document.createElement('h2')
-  summary.textContent = `You have ${incompleteTodos.length} todos left`
-  document.querySelector('#todos').appendChild(summary)
-
-  filteredTodos.forEach(function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('#todos').appendChild(p)
-  })
 }
 
 renderTodos(todos, filters)
@@ -79,7 +18,7 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
     text: e.target.elements.text.value,
     completed: false
   })
-  localStorage.setItem('todos', JSON.stringify(todos))
+  saveTodos(todos)
   renderTodos(todos, filters)
   e.target.elements.text.value = ''
 })
