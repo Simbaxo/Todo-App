2390ff2ce7500d37484a812e875ea1454a443470
Andres Mena <ballerlikemike23@gmail.com>
2019-01-02 21:02:42 -0500

    Remove a todo by id

--- todo-functions.js
+++ todo-functions.js
@@ -14,6 +14,17 @@ const saveTodos = function (todos) {
   localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+// Remove a todo by id
+const removeTodo = function (id) {
+  const todoIndex = todos.findIndex(function (todo) {
+    return todo.id === id
+  })
+
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1)
+  }
+}
+
 // Render application todos based on filters
 const renderTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
@@ -52,6 +63,11 @@ const generateTodoDOM = function (todo) {
   // Setup the remove button
   removeButton.textContent = 'x'
   todoEl.appendChild(removeButton)
+  removeButton.addEventListener('click', function () {
+    removeTodo(todo.id)
+    saveTodos(todos)
+    renderTodos(todos, filters)
+  })
 
   return todoEl
 }
