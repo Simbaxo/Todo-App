d105149fa70f6bad7c643f2a1b7280d5fd42cccd
Andres Mena <ballerlikemike23@gmail.com>
2019-01-04 21:23:14 -0500

    Update empty message

--- scripts/todo-functions.js
+++ scripts/todo-functions.js
@@ -55,7 +55,7 @@ const renderTodos = (todos, filters) => {
   } else {
     const messageEl = document.createElement('p')
     messageEl.classList.add('empty-message')
-    messageEl.textContent = 'No todos to show'
+    messageEl.textContent = 'There are no todos to show'
     todoEl.appendChild(messageEl)
   }
 }
