eb09f69aa5d553179b526df651647c33e7e4f280
Andres Mena <ballerlikemike23@gmail.com>
2019-01-23 23:49:14 -0500

    Refactor completed!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-import { getFilters, setFilters } from './filters'
-import { getTodos, createTodo } from './todos'
+import { setFilters } from './filters'
+import { createTodo } from './todos'
+import { renderTodos } from './views'
 
-console.log(getTodos())
-createTodo('Finish another video')
-console.log(getTodos())
+renderTodos()
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+  setFilters({
+    searchText: e.target.value
+  })
+  renderTodos()
+})
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+  const text = e.target.elements.text.value.trim()
+  e.preventDefault()
+
+  if (text.length > 0) {
+    createTodo(text)
+    renderTodos()
+    e.target.elements.text.value = ''
+  }
+})
+
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+  setFilters({
+    hideCompleted: e.target.checked
+  })
+  renderTodos()
+})
